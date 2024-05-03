2431c9689e211a387986a911d6dadef2c332edeb
DiegoBruno <129461553+diegobrunoml@users.noreply.github.com>
2024-05-02 22:51:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('open_slide_menu').addEventListener('click', function() {
+    document.getElementById('slide_menu').classList.toggle('visible');
+
+    document.getElementById('slide_menu').classList.toggle('hidden');
+
+});
+
